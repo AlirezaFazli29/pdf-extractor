fefdfe35c941ddfc3ad0cf9c978acd0fa60c75e8
AlirezaFazli29 <alirezafazli29@gmail.com>
2025-11-03 16:34:46 +0330

    extract images from docs with PyMu

--- app/core/extractor.py
+++ app/core/extractor.py
@@ -1,4 +1,5 @@
 import fitz
+import base64
 from .utils import normalize_digits_and_fix_order
 from concurrent.futures import ProcessPoolExecutor
 
@@ -28,6 +29,45 @@ class MuExtractor:
             "page_number": pg_num + 1,
             "text": pg_txt
         }
+    
+    @staticmethod
+    def _extract_image(
+        args,
+    ) -> dict:
+        file_path, pg_num = args
+        with fitz.open(file_path) as doc:
+            pg_imgs = doc.get_page_images(pg_num)
+            imgs_list = []
+            for tup_img in pg_imgs:
+                try: 
+                    xref = tup_img[0]
+                    temp_dict = doc.extract_image(xref)
+                    image_bytes = temp_dict.get("image")
+                    base64_str = base64.b64encode(image_bytes).decode("utf-8")
+                    res = {
+                        "image_name": tup_img[7],
+                        "width_px": temp_dict.get("width"),
+                        "height_px": temp_dict.get("height"),
+                        "file_extension": temp_dict.get("ext"),
+                        "image_size_bytes": temp_dict.get("size"),
+                        "Bits_per_color_component": temp_dict.get("bpc"),
+                        "compression_method": tup_img[-1],
+                        "color_space": temp_dict.get("cs-name"),
+                        "bounding_box": {
+                            "x0": doc[pg_num].get_image_rects(xref)[0][0],
+                            "y0": doc[pg_num].get_image_rects(xref)[0][1],
+                            "x1": doc[pg_num].get_image_rects(xref)[0][2],
+                            "y1": doc[pg_num].get_image_rects(xref)[0][3],
+                        },
+                        "image_base64": base64_str,
+                    }
+                    imgs_list.append(res)
+                except:
+                    continue
+            return {
+                "page_number": pg_num + 1,
+                "images": imgs_list
+            }
 
     def extract_text(
             self,
@@ -58,6 +98,34 @@ class MuExtractor:
 
         return results
     
+    def extract_image(
+            self,
+            max_workers: int = 64,
+    ) -> list[dict]:
+        results = []
+
+        for i in range(0, self.page_count, max_workers):
+            chunk_args = [
+                (self.file_path, j)
+                for j in range(i, min(i + max_workers, self.page_count))
+            ]
+
+            with ProcessPoolExecutor(
+                max_workers=min(max_workers, len(chunk_args))
+            ) as executor:
+                chunk_results = list(
+                    executor.map(
+                        self._extract_image,
+                        chunk_args,
+                    )
+                )
+
+            results.extend(chunk_results)
+        
+        results.sort(key=lambda x: x["page_number"])
+
+        return results
+
     def get_metadata(
             self,
     ) -> dict:
