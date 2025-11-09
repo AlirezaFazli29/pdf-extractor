5d747184b280e46cd68367f77644f168c5c281c4
AlirezaFazli29 <alirezafazli29@gmail.com>
2025-11-09 10:55:20 +0330

    creating a dedicated function for eng_numbering

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,11 +1,6 @@
 import re
 
-# Maps for Arabic-Indic and Eastern Arabic-Indic (Persian) digits -> Western digits
-ARABIC_INDIC_MAP = {ord(c): str(i) for i, c in enumerate("٠١٢٣٤٥٦٧٨٩")}   # U+0660..U+0669
-PERSIAN_MAP = {ord(c): str(i) for i, c in enumerate("۰۱۲۳۴۵۶۷۸۹")}        # U+06F0..U+06F9
 
-# Combined translation table
-TRANSLATE_TABLE = {**ARABIC_INDIC_MAP, **PERSIAN_MAP}
 
 def normalize_digits_and_fix_order(
         text: str,
@@ -18,6 +13,16 @@ def normalize_digits_and_fix_order(
     )
 
     if eng_numbering:
-        normalizeed_text = normalizeed_text.translate(TRANSLATE_TABLE)
+        normalizeed_text = digits_to_latin(normalizeed_text)
 
     return normalizeed_text
+
+def digits_to_latin(text: str):
+    # Maps for Arabic-Indic and Eastern Arabic-Indic (Persian) digits -> Western digits
+    ARABIC_INDIC_MAP = {ord(c): str(i) for i, c in enumerate("٠١٢٣٤٥٦٧٨٩")}
+    PERSIAN_MAP = {ord(c): str(i) for i, c in enumerate("۰۱۲۳۴۵۶۷۸۹")}
+
+    # Combined translation table
+    TRANSLATE_TABLE = {**ARABIC_INDIC_MAP, **PERSIAN_MAP}
+
+    return text.translate(TRANSLATE_TABLE)
